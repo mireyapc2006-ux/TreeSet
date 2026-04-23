552c968e76183f5c1855cf3284f03937d515db64
Ancor1234 <ancor.perez103@alu.ulpgc.es>
2026-04-23 16:23:51 +0100

    Update TreeSet.py

--- TreeSet.py
+++ TreeSet.py
@@ -2,12 +2,16 @@ from Node import Node
 
 class TreeSet:
     def __init__(self, value = None):
-        if value != None:
+        if value is not None:
             self.__root = Node(value)
             self.__size = 1
+            self.__first = self.__root
+            self.__last = self.__root
         else:
             self.__root = None
             self.__size = 0
+            self.__first = None
+            self.__last = None
     
     @property
     def root(self):
@@ -15,4 +19,48 @@ class TreeSet:
     
     def size():
         return self.__size
+    
+    def add(self, value):
+        if (self.__root is None):
+            self.__root = Node(value)
+        else:
+            __add_value(self.__root, value)
+        
+        self.__size += 1
+
+    def __add_value(self, parent, value):
+        if (value < parent.value):
+            if parent.left is None:
+                parent.left = Node(value)
+                return
+            else:
+                return __add_value(parent.left, value)
+        
+        elif (value > parent.value):
+            if parent.right is None:
+                parent.right = Node(value)
+                return
+            else:
+                return __add_value(parent.right, value)
+        
+    
+    def is_empty(self):
+        return self.__size == 0
+
+    @property
+    def first():
+        return self.__first
+    
+    def first():
+        return self.__first
+    
+    @property
+    def last():
+        return self.__last
+    
+    def last():
+        return self.__last
+    
+    def __iter__(self):
+        pass
 
