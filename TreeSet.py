a6ea9df05b02a9626de4a422b6bab4d7b8b2a202
Ancor1234 <ancor.perez103@alu.ulpgc.es>
2026-04-23 15:49:23 +0100

    Update TreeSet.py

--- TreeSet.py
+++ TreeSet.py
@@ -1,1 +1,18 @@
+from Node import Node
+
+class TreeSet:
+    def __init__(self, value = None):
+        if value != None:
+            self.__root = Node(value)
+            self.__size = 1
+        else:
+            self.__root = None
+            self.__size = 0
+    
+    @property
+    def root(self):
+        return self.__root
+    
+    def size():
+        return self.__size
 
